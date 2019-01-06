768bb3f5cd60889e2b29a0b670a813edf80ea5e2
Pathagama Kuruppuge Tharindu <pktharindu@outlook.com>
2019-01-06 16:26:18 +0530

    install laravel passport

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -2,13 +2,15 @@
 
 namespace App;
 
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable;
+    use Notifiable,
+        HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->middleware('verified')->name('home');
