512e1a8072245393b2a90396e9b30839d09b4fcc
P. K. Tharindu <pktharindu@outlook.com>
2020-11-11 09:02:31 +0530

    wip

--- nuxt/middleware/guest.js
+++
@@ -1,5 +0,0 @@
-export default function({ store, redirect }) {
-  if (store.state.auth.loggedIn) {
-    return redirect('/')
-  }
-}
--- nuxt/pages/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <section class="container">
-    <div>
-      <strong>Home Page</strong>
-      <pre>Both guests and logged in users can access!</pre>
-      <nuxt-link to="/login">Login</nuxt-link>
-    </div>
-  </section>
-</template>
